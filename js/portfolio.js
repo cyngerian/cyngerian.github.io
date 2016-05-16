a20e61df724e49aa34c10b3721c57470f27524c2
Ian Trancynger <itrancynger@gmail.com>
2016-05-16 02:19:58 -0400

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,18 @@
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
- $('.navbar-collapse ul li a').click(function(){ 
+$('.navbar-collapse ul li a').click(function(){ 
             $('.navbar-toggle:visible').click();
     });
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
